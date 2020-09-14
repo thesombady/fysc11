4a69cf61c0c76d5ed832c247ae41ba65761c616e
Andreas Evensen <andreas.evensen11@gmail.com>
2020-09-14 11:58:31 +0200

    commit

--- excerise1.py
+++ excerise1.py
@@ -24,24 +24,14 @@ print(BindingEnergy(11, 5, 0) - BindingEnergy(11, 6, 0))
 EnergyValue = np.array([0.97, 1.2, 1.73, 1.74, 3.03, 3.25, 3.92, 4.38, 4.94, 5.51, 5.47])
 
 Nucleinumber = [(6, 11), (7, 13), (8, 15), (9, 17), (12, 23), (13, 25), (15,29), (16, 31), (18,35), (20,39), (21, 41)]
-"""
-XAxis = []
-for i in range(len(Nucleinumber)):
-    Value = Nucleinumber[i][1]
-    Energy = ColumbEnergy(Value)
-    XAxis.append(Energy)
-"""
+
 XAxis = np.array([ColumbEnergy(Nucleinumber[i][1]) for i in range(len(Nucleinumber))])
-
-
-
 Regression = stats.linregress(XAxis, EnergyValue)
-print(1/Regression[0])
-print(Regression)
+
 plt.title('Beta - decay')
 plt.plot(XAxis, EnergyValue, marker = '.', linestyle = '', label = 'Orignal Data')
 plt.plot(XAxis, Regression[1] + Regression[0] * XAxis, label = 'Fitted')
-plt.text(6, 1, f'Error of fitted line is {Regression[4]}')
+plt.text(6, 1, r'$\Delta$ r = 0.01933195908850164')
 plt.text(7, 2, f'y = {1/Regression[0]}x + {Regression[1]}')
 plt.legend(loc = 'best')
 plt.xlabel('Nuclei')
