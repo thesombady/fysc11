ace8d668dedd77af78fbc807a908b1556c7b3972
Andreas Evensen <andreas.evensen11@gmail.com>
2020-09-14 14:17:53 +0200

    test

--- excerise1.py
+++ excerise1.py
@@ -1,21 +1,21 @@
 import matplotlib.pyplot as plt
 from scipy import stats
 import numpy as np
+c1 = 15.9
+c2 = 18.4
+c3 = 0.71
+c4 = 23.2
+c5 = 11.5
 
 def BindingEnergy(A, Z, Pair = 1):
-    c1 = 15.9
-    c2 = 18.4
-    c3 = 0.71
-    c4 = 23.2
-    c5 = 11.5
     return c1 * A - c2 * A **(2/3) - c3 * Z**2 / (A**(1/3)) - c4 * (A - 2*Z)**2 / A + c5 * Pair/ (A)**(1/2)
-"""
-for i in range(0, 44, 2): #Calcium 62 does not exits because it's below neutron dripline.
+
+for i in range(0, 44, 2): #Calcium 60 does not exits because it's below neutron dripline.
     print(BindingEnergy(i + 20, 20) - BindingEnergy(i + 19, 20), i + 20)
 print("---------------------------------------------")
 for i in range(0, 44, 2):#36
     print(BindingEnergy(i + 20, 20) - BindingEnergy(i + 19, 20 - 1), i + 20)
-"""
+
 
 def ColumbEnergy(Z):
     K = 1
@@ -39,4 +39,4 @@ plt.text(7, 2, f'y = {1/Regression[0]}x + {Regression[1]}')
 plt.legend(loc = 'best')
 plt.xlabel('Nuclei')
 plt.ylabel(r'$Q_{EC}$')
-plt.show()
+#plt.show()
