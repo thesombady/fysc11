57f968cc0950ca981c17dea86faa6cbfe8e067d2
Andreas Evensen <andreas.evensen11@gmail.com>
2020-09-24 14:19:23 +0200

    commit

--- Zeeman.py
+++ Zeeman.py
@@ -1,45 +1,40 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from sklearn.linear_model import LinearRegression
 
-
-"""
-def FineEnergy(n,j,z):
-    Energy = -13.6 / n**2 * z**2 * (1 + (1/137)**2 / n ** 2 * (n /(j + 1/2) - 3/4))
-    return Energy
-
-def WaveConvertion(Energy):
-    WaveLenght = (Energy * 1.602*10**(-19) / (6.626 * 10 **(-34) * 2.99 * 10 **8)) ** (-1)
-    return WaveLenght
-
-Energy = FineEnergy(5, 2, 2) - FineEnergy(6, 1, 2)
-print(WaveConvertion(Energy))
-print(Energy)
-#Energy = FineEnergy(5, 1/2, 2) - FineEnergy(6, 1/2, 2)
-#print(WaveConvertion(Energy))
-#print(Energy)
-"""
 global D1, D2, ub
 
-D1 = 0.5309 #um
-D2 = 0.8827 #um
+D2 = 358.05 #um
+D1 = 221.073 #um
 ub = 5.8 * 10 ** (-5) #eV/T
-#D1 = 215 #pixel
-#D2 = 352 #pixel
 
 
 def Sigma(Da, Db):
-    return 1/(2 * 3.085) * ( Da ** 2 - Db ** 2) /(D1 ** 2 - D2 ** 2)
+    return 1/(2 * 3.085) * ( Da ** 2 - Db ** 2) /(D2 ** 2 - D1 ** 2) * 0.00124
 
 def BField(Current):
     return Current * 0.1
 
-CurrentList = [0.25, 0.5, 0.75, 1, 1.25 , 1.5, 1.75, 2, 2.25, 2.5, 2.75, 3, 3.25, 3.5, 3.75]#, 4, 4.25, 4.5, 4,75, 5]
-#           0.25    0.5     0.75    1       1.25    1.5     1.75    2       2.25    2.5     2.75    3       3.25    3.5     3.75
-DaValues = [0.9132, 0.5962, 0.6020, 0.5881, 0.6403, 0.6586, 0.6689, 0.6696, 0.6912, 0.8058, 0.7761, 0.7557, 0.7466, 0.7601, 0.7501]
-DbValues = [0.5554, 0.5020, 0.4966, 0.4629, 0.4642, 0.4301, 0.4149, 0.3911, 0.3684, 0.7303, 0.7136, 0.5738, 0.5572, 0.5831, 0.5798]
+def LinRegress(Param):
+    pass
 
-BfieldValues = [BField(CurrentList[i]) for i in range(len(CurrentList))]
-SigmaValues = [Sigma(DbValues[i], DaValues[i]) for i in range(len(DaValues)) if len(DaValues) == len(DbValues)]
+
+CurrentList = [0.25, 0.5, 0.75, 1, 1.25, 1.5, 1.75, 2, 2.25, 2.5, 2.75, 3, 3.25, 3.5, 3.75, 4, 4.25, 4.5, 4.75, 5]
+DaValues = [231.572, 244.285, 242.868, 253.953, 254.487, 265.238, 278.957, 280.347, 281.347, 283.003, 287.615, 314.590, 314.590, 314.590, 318.309, 322.040, 323.306, 331.110, 331.539, 342.539]
+DbValues = [209.178, 193.743, 181.697, 178.473, 170.455, 161.709, 155.952, 143.231, 138.367, 147.544, 141.047, 127.422, 115.089, 114.300, 114.846, 133.873, 138.417, 138.922, 143.012, 152.539]
+BfieldValues = np.array([BField(CurrentList[i]) for i in range(len(CurrentList))])
+SigmaValues = np.array([Sigma(DaValues[i], DbValues[i]) for i in range(len(DaValues)) if len(DaValues) == len(DbValues)])
+
+XValues = BfieldValues.reshape((-1,1))
+
+Model = LinRegress().fit(XValues, SigmaValues)
+#r_sq = Model.score(x, y)
+print(Model)
+#print('coefficient of determination:', r_sq)
+
 
 plt.plot(BfieldValues, SigmaValues, '.')
-plt.show()
+plt.xlabel(r'Magnetic field $B$')
+plt.ylabel(r'$\Delta \sigma$ [eV]')
+plt.title("Transistion spectrum with doppler effect")
+#plt.show()
