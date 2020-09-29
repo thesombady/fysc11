f8d6dd719d2b0914137654996246f3d92d29e1ec
Andreas Evensen <andreas.evensen11@gmail.com>
2020-09-29 12:18:33 +0200

    commit

--- Computer_project.py
+++ Computer_project.py
@@ -1,2 +1,58 @@
-import os, sys
+"""
+Preperations
+"""
 import numpy as np
+import scipy
+import matplotlib.pyplot as plt
+import sys
+import sympy
+import math
+#The following two path extensions are located in the same directory as the main-file.
+sys.path.append('radial.py')
+sys.path.append('radiallog.py')
+import radial
+import radiallog
+#print(sys.path)
+
+
+
+"""
+Task One
+"""
+Z = 1
+r = np.linspace(0, 35, 35 * 120)
+P1s = 2 * r * np.exp(-r * Z) * Z ** (3/2)
+P2s = 1/math.sqrt(2) * r * np.exp(-r * Z/2) * (1 - 1/2 * Z*r)
+P2p = 1/(2 * math.sqrt(6)) * r ** 2 * np.exp(-r * Z/2) * Z ** (5/2)
+P3s = 2/(3 * math.sqrt(3)) * Z ** (3/2) * r * np.exp(-r * Z / 3) * (1 - 2/3 * Z * r + 2/27 * Z ** 2 * r ** 2)
+P3p = 8/(27 * math.sqrt(6)) * Z ** (5/3) * r ** 2 * np.exp(-r * Z/3) * (1 - 1/6 * Z * r)
+P3d = 4/(81 * math.sqrt(30)) * Z ** (7/2) * r ** 3  * np.exp(-r * Z/3)
+plt.plot(r, P1s, 'b', label = 'P1s state')
+plt.plot(r, P2s, 'b', label = 'P2s state')
+plt.plot(r, P2p, 'r', label = 'P2p state')
+plt.plot(r, P3s, 'b', label = 'P3s state')
+plt.plot(r, P3p, 'r', label = 'P3p state')
+plt.plot(r, P3d, 'g', label = 'P3d State')
+plt.title('Probability distribution for given state')
+plt.ylabel(r'$P(r)$')
+plt.xlabel(r'$r$')
+plt.legend()
+plt.show()
+
+"""
+Task 2
+"""
+z = (1,2,3,4)
+r = np.linspace(0, 10, 10 * 120)
+for Z in z:
+    P1s =  2 * r * np.exp(-r * Z) * Z ** (3/2)
+    plt.plot(r, P1s, label = f'P1s state for {Z}')
+plt.legend()
+plt.title(r'Distrubution for a given $Z$ value')
+plt.ylabel(r'$P(r)$')
+plt.xlabel(r'$r$')
+plt.show()
+
+"""
+Task 3
+"""
